a16c255283142005763c483db2eb34265f45e4ee
Sevris Michael <msevris@hotmail.com>
2020-06-23 12:29:19 +0300

    AdminUser Migration

--- BlazorMovies/Server/ApplicationDbContext.cs
+++ BlazorMovies/Server/ApplicationDbContext.cs
@@ -1,6 +1,8 @@
 ﻿using BlazorMovies.Shared.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace BlazorMovies.Server
 {
@@ -13,6 +15,32 @@ namespace BlazorMovies.Server
         {
             modelBuilder.Entity<MoviesActors>().HasKey(x => new { x.MovieId, x.PersonId });
             modelBuilder.Entity<MoviesGenres>().HasKey(x => new { x.MovieId, x.GenreId });
+            // *** IT IS CORRECT 
+            // but we use Sql Query instead 
+
+            //var roleAdminId = "57f0e373-2d6f-4fff-90a2-c151d00c2410";
+            //var userAdminId = "04aea431-6c56-4892-bc63-5929b77569ea";
+            //var hasher = new PasswordHasher<IdentityUser>();
+            //var userAdmin = new IdentityUser()
+            //{
+            //    Id = userAdminId,
+            //    Email = "michael@hotmail.com",
+            //    UserName = "michael@hotmail.com",
+            //    NormalizedEmail = "michael@hotmail.com",
+            //    NormalizedUserName = "michael@hotmail.com",
+            //    EmailConfirmed = true,
+            //    PasswordHash = hasher.HashPassword(null, "exPr0f!t")
+            //};
+
+            //modelBuilder.Entity<IdentityUser>().HasData(userAdmin);
+            //modelBuilder.Entity<IdentityUserClaim<string>>().HasData(new IdentityUserClaim<string>()
+            //{
+            //    Id = 1,
+            //    ClaimType = ClaimTypes.Role,
+            //    ClaimValue = "Admin",
+            //    UserId = userAdminId
+            //});
+
 
             base.OnModelCreating(modelBuilder);
         }
---
+++ BlazorMovies/Server/Migrations/20200623092116_AdminUser.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlazorMovies.Server.Migrations
+{
+    public partial class AdminUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+            @"
+                IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'AccessFailedCount', N'ConcurrencyStamp', N'Email', N'EmailConfirmed', N'LockoutEnabled', N'LockoutEnd', N'NormalizedEmail', N'NormalizedUserName', N'PasswordHash', N'PhoneNumber', N'PhoneNumberConfirmed', N'SecurityStamp', N'TwoFactorEnabled', N'UserName') AND [object_id] = OBJECT_ID(N'[AspNetUsers]'))
+                    SET IDENTITY_INSERT [AspNetUsers] ON;
+                INSERT INTO [AspNetUsers] ([Id], [AccessFailedCount], [ConcurrencyStamp], [Email], [EmailConfirmed], [LockoutEnabled], [LockoutEnd], [NormalizedEmail], [NormalizedUserName], [PasswordHash], [PhoneNumber], [PhoneNumberConfirmed], [SecurityStamp], [TwoFactorEnabled], [UserName])
+                VALUES (N'04aea431-6c56-4892-bc63-5929b77569ea', 0, N'a08061f7-6217-4e96-a104-bed7d98d50de', N'michael@hotmail.com', CAST(1 AS bit), CAST(0 AS bit), NULL, N'michael@hotmail.com', N'michael@hotmail.com', N'AQAAAAEAACcQAAAAEDRbCuvzt5/eIVVPl1+VlP9YJlye1zvBneRX83HXD42u/ZXEd6F0/RFty/DDn4AngQ==', NULL, CAST(0 AS bit), N'ec4b6ef9-3bb6-412e-a0e2-d5b8c4bfb945', CAST(0 AS bit), N'michael@hotmail.com');
+                IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'AccessFailedCount', N'ConcurrencyStamp', N'Email', N'EmailConfirmed', N'LockoutEnabled', N'LockoutEnd', N'NormalizedEmail', N'NormalizedUserName', N'PasswordHash', N'PhoneNumber', N'PhoneNumberConfirmed', N'SecurityStamp', N'TwoFactorEnabled', N'UserName') AND [object_id] = OBJECT_ID(N'[AspNetUsers]'))
+                    SET IDENTITY_INSERT [AspNetUsers] OFF;
+                
+                GO
+                
+                IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'ClaimType', N'ClaimValue', N'UserId') AND [object_id] = OBJECT_ID(N'[AspNetUserClaims]'))
+                    SET IDENTITY_INSERT [AspNetUserClaims] ON;
+                INSERT INTO [AspNetUserClaims] ([Id], [ClaimType], [ClaimValue], [UserId])
+                VALUES (1, N'http://schemas.microsoft.com/ws/2008/06/identity/claims/role', N'Admin', N'04aea431-6c56-4892-bc63-5929b77569ea');
+                IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'ClaimType', N'ClaimValue', N'UserId') AND [object_id] = OBJECT_ID(N'[AspNetUserClaims]'))
+                    SET IDENTITY_INSERT [AspNetUserClaims] OFF;
+                
+                GO
+                
+                INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+                VALUES (N'20200623092116_AdminUser', N'3.1.5');
+                
+                GO
+                
+             ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUserClaims",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "04aea431-6c56-4892-bc63-5929b77569ea");
+        }
+    }
+}
